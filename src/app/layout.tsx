ac67a9565bf9f4127052a60b7aa4a0054ee8e7fc
Rene Nuñez <renenunezgalaviz@gmail.com>
2026-04-14 16:35:13 -0700

    Prevent horizontal page overflow by clipping body overflow-x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${lora.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col overflow-x-hidden">
         <Nav />
         {children}
       </body>
